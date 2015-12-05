e4167e82c75a4314a7404698911c5ca9b1abc0f1
Amy Dewaal <amy.dewaal@covenant.edu>
2015-12-05 14:40:42 -0500

    added Apache license notice to files under /app/

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name: 'test', email: 'test@test.com', password: 'testing')
+User.create(name: 'linda', email: 'linda@test.com', password: 'notthereyo')
+User.create(name: 'lee', email: 'lee@test.com', password: 'helloworld')
+
+UsersCourse.create(user_id: 1, course_id: 18, taken_planned: '2015f', taken_on: '2015-09-27')
+UsersCourse.create(user_id: 1, course_id: 19, taken_planned: '2015s', taken_on: '2015-03-13')
+UsersCourse.create(user_id: 1, course_id: 21, taken_planned: '2016s')
+UsersCourse.create(user_id: 1, course_id: 23, taken_planned: '2015f')
+UsersCourse.create(user_id: 1, course_id: 270, taken_planned: '2017f')
+UsersCourse.create(user_id: 1, course_id: 271, taken_planned: '2017f')
+UsersCourse.create(user_id: 1, course_id: 273)
+
+UsersMajor.create(user_id: 1, major_id: 1)
+UsersMajor.create(user_id: 1, major_id: 2)
+
+MajorsCourse.create(major_id: 1, course_id: 18)
+MajorsCourse.create(major_id: 1, course_id: 19)
+MajorsCourse.create(major_id: 1, course_id: 20)
+MajorsCourse.create(major_id: 1, course_id: 21)
+MajorsCourse.create(major_id: 1, course_id: 23)
+MajorsCourse.create(major_id: 1, course_id: 100)
+
+MajorsCourse.create(major_id: 2, course_id: 270)
+MajorsCourse.create(major_id: 2, course_id: 271)
+MajorsCourse.create(major_id: 1, course_id: 273)
+MajorsCourse.create(major_id: 2, course_id: 500)
+
+Major.create(name: "Art", total_hours: 120)
+Major.create(name: "Computer Science", total_hours: 120)
+
+Distribution.create(title: "Humanities")
+Distribution.create(title: "Fine Arts")
+DistributionsCourse.create(distribution_id: 2, course_id: 21)
+
+
+
+
