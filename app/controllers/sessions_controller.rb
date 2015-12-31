a939bf12087f9c1e49fa5ab070ebaeea5e90ac22
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2015-12-31 01:14:56 -0800

    changed sessions controller to facilitate sessions through cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,22 +16,41 @@
 
 class SessionsController < ApplicationController
 
+  # def new
+  #   if session[:user_id]
+  #     redirect_to '/welcome'
+  #   end
+  # end
+  
   def new
-    if session[:user_id]
+    if cookies.permanent[:auth_token]
       redirect_to '/welcome'
     end
   end
 
   def create
+    # user = User.find_by_email(params[:email])
+    # # If the user exists AND the password entered is correct.
+    # if user && user.authenticate(params[:password])
+    #   # Save the user id inside the browser cookie. This is how we keep the user
+    #   # logged in when they navigate around the website.
+    #   session[:user_id] = user.id
+    #   redirect_to '/welcome'
+    # else
+    #   # If user's login doesn't work, send them back to the login form.
+    #   flash[:danger] = "The email/password you entered is incorrect. Please try again."
+    #   redirect_to '/login'
+    # end
+    
     user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user
-      # logged in when they navigate around the website.
-      session[:user_id] = user.id
+      if params[:remember_me]
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
       redirect_to '/welcome'
     else
-      # If user's login doesn't work, send them back to the login form.
       flash[:danger] = "The email/password you entered is incorrect. Please try again."
       redirect_to '/login'
     end
@@ -39,7 +58,7 @@ class SessionsController < ApplicationController
 
   # Log out
   def destroy
-    session[:user_id] = nil
+    cookies.delete(:auth_token)
     redirect_to '/login'
   end
 
