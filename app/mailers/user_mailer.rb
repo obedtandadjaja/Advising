08a08712817acd70c3f9b88e9bd98a2a8b817943
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-01-20 21:15:20 -0500

    added another function in usermailer to send admin invites

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,13 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset(user)
-    @user = user
-    mail :to => user.email, :subject => "Covenant College Advising Password Reset"
-  end
+  	def password_reset(user)
+    	@user = user
+    	mail :to => user.email, :subject => "Covenant College Advising Password Reset", :from => "admin@covenantadvising.edu"
+  	end
+
+  	def invite(user)
+    	@user = user
+    	mail :to => user.email, :subject => "Covenant College Advising Invite", :from => "admin@covenantadvising.edu"
+  	end
+
 end
