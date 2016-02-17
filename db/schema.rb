afd1cd15d5884bb52983119a55a71252727fa752
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-02-17 01:45:41 -0500

    added user confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -117,6 +117,10 @@ ActiveRecord::Schema.define(version: 20160217013810) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email",      limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
